32696aa081f2af4b75a64d6d01db8752ae0de911
BradMJ <90422753+BradMJ@users.noreply.github.com>
2024-01-04 14:02:19 -0800

    Initial commit with task class

---
+++ src/modules/tasks.js
@@ -0,0 +1,16 @@
+export default class Task {
+    constructor(name) {
+        this.name = name;
+    };
+
+    setName(name) {
+        this.name = name;
+    };
+
+    getName() {
+        return this.name;
+    };
+
+    // Add due dates
+
+};
