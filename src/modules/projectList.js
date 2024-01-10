8f2d1b53838cf2f6cbc578848081a290a05c2bc5
BradMJ <90422753+BradMJ@users.noreply.github.com>
2024-01-10 14:47:46 -0800

    Initial commit including class working with project list

---
+++ src/modules/projectList.js
@@ -0,0 +1,37 @@
+import Project from './projects.js';
+import Task from './tasks.js';
+
+export default class ProjectList {
+    constructor() {
+        this.projects = [];
+    }
+
+    setProjects(projects) {
+        this.projects = projects;
+    };
+
+    getProjects() {
+        return this.projects;
+    };
+
+    getProject(projectName) {
+        return this.projects.find((project) => project.getName() === projectName);
+    };
+
+    contains(projectName) {
+        return this.projects.some((project) => project.getName() === projectName);
+    };
+
+    addProject(newProject) {
+        if (this.projects.find((project) => project.name === newProject.name)) return;
+        this.projects.push(newProject);
+    };
+
+    deleteProject(projectName) {
+        const projectToDelete = this.projects.find((project) => project.getName() === projectName);
+        this.projects.splice(this.projects.indexOf(projectToDelete), 1);
+    };
+
+    // Sort tasks methods here
+    
+};
