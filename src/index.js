373e1de8d53a0efad53e155eb7ea0f9d1000db39
BradMJ <90422753+BradMJ@users.noreply.github.com>
2024-01-31 15:13:33 -0800

    Import UI module to load page content

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import Project from "./modules/projects.js";
-import Task from "./modules/tasks.js";
+import UI from "./modules/ui.js";
 
-// Test
-const cleanHouse = new Project('Clean house');
-console.log(cleanHouse);
-
-const sweepFloor = new Task('Sweep floor');
-console.log(sweepFloor);
+document.addEventListener('DOMContentLoaded', UI.loadHomepage);
